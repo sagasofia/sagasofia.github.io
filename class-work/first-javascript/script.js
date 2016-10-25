cea55c849cf252f3e2e1b1c624734011972f80d4
sagasofia <slundquist@Saga-lundqvist-MacBook-Pro.local>
2016-10-25 16:34:25 -0400

    javascript 1

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,25 @@
+alert('This is an alert. This is annoying');
+
+	document.write('Hello, stranger!');
+	document.write('<h1>' + 10 * 10 + '<h1/>');
+	document.write('<h2>' + (50 - 17) + '<h2/>');
+	var h1 = '<h1>' + 10 * 10 + '<h1/>';
+	var h2 = '<h2>' + 50 - 17 + '<h2/>';
+
+	document.write(h1);
+	document.write(h2);
+
+function hello () {
+	  var greeting = 'hello!';
+
+	  greeting = 'hellooo, world!';
+
+	  alert(greeting);
+}
+
+hello ();
+
+document.getElementById('title').style.color = 'red';
+
+
+document.querySelector('text-chunk').style.color = 'red';
